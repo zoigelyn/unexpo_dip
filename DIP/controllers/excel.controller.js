869a1be644e0190eaca4a4008234a546578a1a7b
unknown <zoigelyn@gmail.com>
2021-02-12 03:16:57 -0430

    m

--- DIP/controllers/excel.controller.js
+++ DIP/controllers/excel.controller.js
@@ -49,7 +49,7 @@ module.exports.insertarBaseE = async function (req, res, next) {
   try {
     let bandera = 0;
     // si se realiza la carga de archivo exitosamente se gernera un objeto file, el cual es accesible desde req, y en el se encuentra toda la informacion del archivo cargado
-    let ruta = path.join(__dirname, '..\\noPublic\\uploads\\', req.file.filename)//ruta del archivo
+    let ruta = path.join(__dirname, '../noPublic/uploads/', req.file.filename)//ruta del archivo
 
     readXlsxFile(ruta).then(async function (rows) {//el middleware toma el archivo de la ruta y extrae las filas
       rows.shift(); //Se desplaza el primer elemento, el encabezado
@@ -120,8 +120,8 @@ module.exports.insertarBaseE = async function (req, res, next) {
 
 
       if (creacion) { //Si se realiza la creacion se buscan usuarios con el tipo de usuario "estudiante"
-        /*let filePath = path.join(__dirname, '..\\noPublic\\uploads\\', req.file.filename);
-        fs.unlinkSync(filePath);*/
+        let filePath = path.join(__dirname, '..\\noPublic\\uploads\\', req.file.filename);
+        fs.unlinkSync(filePath);
         const usuariosE = await Usuarios.findAll({//Se realiza una busqueda en el modelo Estudiante con el metodo findAll proporcionado por sequelize
           where: {
             tipo_u: 'estudiante'// la busqueda se condiciona
@@ -275,8 +275,8 @@ module.exports.insertarBaseD = async function (req, res, next) {
 
 
       if (creacion) {
-       /* let filePath = path.join(__dirname, '..\\noPublic\\uploads\\', req.file.filename); //si se realiza la creacion se procede a eliminar el archivo excel, debido a que no es necesario su almacenamiento despues de haber importado los datos en la base de datos
-        fs.unlinkSync(filePath);*/
+        let filePath = path.join(__dirname, '..\\noPublic\\uploads\\', req.file.filename); //si se realiza la creacion se procede a eliminar el archivo excel, debido a que no es necesario su almacenamiento despues de haber importado los datos en la base de datos
+        fs.unlinkSync(filePath);
         var usuariosD = await Usuarios.findAll({//consulta del modelo Usuarios
           where: {
             tipo_u: 'docente'//se condiciona la consulta para usuarios con el tipo de dato docente
